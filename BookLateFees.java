d719e516feb04cce1a308dc59d6f63ca9297859e
Nikita Kodkani <nikita.kodkani@gmail.com>
2017-04-02 20:31:26 +0530

    Create BookLateFees.java

---
+++ BookLateFees.java
@@ -0,0 +1,65 @@
+/*
+Task: 
+Your local library needs your help! Given the expected and actual return dates for a library book, create a program that calculates the fine (if any). The fee structure is as follows:
+
+1. If the book is returned on or before the expected return date, no fine will be charged (i.e.: fine =0).
+2. If the book is returned after the expected return day but still within the same calendar month and year as the expected return date, fine = 15 Hackos * (the number of days late).
+3. If the book is returned after the expected return month but still within the same calendar year as the expected return date, the fine = 500 Hackos *(the number of months late).
+4. If the book is returned after the calendar year in which it was expected, there is a fixed fine of 10000.
+
+INPUT format:
+
+The first line contains 3 space-separated integers denoting the respective day, month, and year on which the book was actually returned. 
+The second line contains 3 space-separated integers denoting the respective day, month, and year  on which the book was expected to be returned (due date).
+
+9 6 2015
+6 6 2015
+
+
+OUTPUT format:
+45
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class BookLateFees {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        /*
+         * Input
+         */
+        int dd1 = in.nextInt();
+        int month1 = in.nextInt();
+        int year1 = in.nextInt();
+        int dd2= in.nextInt();
+        int month2 = in.nextInt();
+        int year2 = in.nextInt();
+        int fine = 0;
+        
+        if(year1 > year2) {
+            fine = 10000;
+        } else if (year1 == year2) {    
+            
+            if (month1 > month2) {
+                fine = (month1 - month2) * 500;                
+            } else if (month1 == month2){
+                if (dd1 > dd2) {
+                    fine = (dd1 - dd2) * 15;
+                } else {
+                    fine = 0;
+                }     
+            } else {
+                fine = 0;
+            }
+        } else {
+            fine = 0;
+        }
+        System.out.println(fine);
+    }
+}
